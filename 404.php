b1cdce40d34e60319fe2b6c7aaded575e0b6f627
Seregin Mikle <mikhail-seregin@yandex.ru>
2019-01-18 13:33:53 +0300

    add 404 page

---
+++ 404.php
@@ -0,0 +1,35 @@
+<? include $_SERVER['DOCUMENT_ROOT'].'/pages/top2.php'?>
+
+<!-- main -->
+<main class="page">
+    <? include $_SERVER['DOCUMENT_ROOT'].'/pages/blocks/aside.php'?>
+    <main class="main">
+        <div class="forbidden">
+            <div class="container2">
+                <div class="forbidden__wrapper">
+                    <div class="forbidden__title">
+                        404
+                    </div>
+                    <div class="forbidden__subtitle">
+                        Извините, мы находимся в процессе творения <br>
+                        этой страницы
+                    </div>
+                    <a href="/" class="forbidden__link">
+                        <div class="btn">
+                            <div class="btn__title">На главную</div>
+                            <div class="btn__lines">
+                                <div class="btn__line-top"></div>
+                                <div class="btn__line-right"></div>
+                                <div class="btn__line-bottom"></div>
+                                <div class="btn__line-left"></div>
+                            </div>
+                        </div>
+                    </a>
+                </div>
+            </div>
+        </div>
+    </main>
+</main>
+<!-- END main -->
+
+<? include $_SERVER['DOCUMENT_ROOT'].'/pages/bottom.php'?>
